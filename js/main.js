7938e7d965c177b4d51a1c4b241ee7f2f03996fd
Philip Protogeridis <ph.prot@gmail.com>
2014-04-14 00:58:03 +0300

    v2

--- js/main.js
+++ js/main.js
@@ -1,3 +1,10 @@
+$(window).load(function() { 
+		$("#status").fadeOut(function(){
+			$('body').removeClass('disabled');
+		}); 
+		$("#preloader").delay(350).fadeOut("slow"); 
+	}); 
+
 $(document).ready(function(){
 
 });
